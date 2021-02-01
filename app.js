9b2195c2305be57ac67b53c9156cf3c5c95e27ff
PatOn <mahomed2278@gmail.com>
2021-02-01 12:11:15 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+// const https = require("https");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/signup.html")
+})
+
+
+app.listen(3000, function() {
+  console.log("Server works on port 3000")
+})
