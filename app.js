8470ed9be9a507a98cb629f5bd972ace61602412
PatOn <mahomed2278@gmail.com>
2021-02-01 13:54:46 +0000

    handling requests
    API server user

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const https = require("https");
+const https = require("https");
 const request = require("request");
 
+
 const app = express();
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({
-  extended: true
-}))
+  extended: true,
+}));
+
+app.post("/", function(req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email
+  console.log(email)
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName,
+      }
+    }]
+  };
+  const jsonData = JSON.stringify(data);
+  const url = "https://us7.api.mailchimp.com/3.0/lists/e19e417b0a";
+  const option = {
+    method: "POST",
+    auth: "Paton30:8bb6b98e01fd3ead9b2c53c025e8d8d7-us7"
+  }
+  const request = https.request(url, option, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+});
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/signup.html")
-})
+  res.sendFile(__dirname + "/signup.html");
+});
 
 
 app.listen(3000, function() {
-  console.log("Server works on port 3000")
-})
+  console.log("Server works on port 3000");
+});
+
+// api key = app
+// 8bb6b98e01fd3ead9b2c53c025e8d8d7-us7
+
+//list audience or something
+// e19e417b0a
